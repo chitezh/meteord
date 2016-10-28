f43173aea6a677dde8b6cbe56d361b208208e3f2
Kingsley Ochu <kochu@c2gconsulting.com>
2016-10-28 12:01:51 +0100

    Revert changes

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -3,18 +3,11 @@ set -x
 
 : ${NODE_VERSION?"NODE_VERSION has not been set."}
 
-docker login -e ochu.kc@gmail.com -u chitezh -p ochu4real
-
-docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "chitezh/meteord:base-node-${NODE_VERSION}" ../base && \
-  docker tag "chitezh/meteord:base-node-${NODE_VERSION}" chitezh/meteord:base
-  docker push chitezh/meteord:base
-docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "chitezh/meteord:onbuild-node-${NODE_VERSION}" ../onbuild && \
-  docker tag "chitezh/meteord:onbuild-node-${NODE_VERSION}" chitezh/meteord:onbuild
-  docker push chitezh/meteord:devbuild
-docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "chitezh/meteord:devbuild-node-${NODE_VERSION}" ../devbuild && \
-  docker tag "chitezh/meteord:devbuild-node-${NODE_VERSION}" chitezh/meteord:devbuild
-docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "chitezh/meteord:binbuild-node-${NODE_VERSION}" ../binbuild && \
-  docker tag "chitezh/meteord:binbuild-node-${NODE_VERSION}" chitezh/meteord:binbuild
-  docker push chitezh/meteord:latest
-
-
+docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:base-node-${NODE_VERSION}" ../base && \
+  docker tag "abernix/meteord:base-node-${NODE_VERSION}" abernix/meteord:base
+docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:onbuild-node-${NODE_VERSION}" ../onbuild && \
+  docker tag "abernix/meteord:onbuild-node-${NODE_VERSION}" abernix/meteord:onbuild
+docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:devbuild-node-${NODE_VERSION}" ../devbuild && \
+  docker tag "abernix/meteord:devbuild-node-${NODE_VERSION}" abernix/meteord:devbuild
+docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:binbuild-node-${NODE_VERSION}" ../binbuild && \
+  docker tag "abernix/meteord:binbuild-node-${NODE_VERSION}" abernix/meteord:binbuild

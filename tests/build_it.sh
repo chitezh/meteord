87718323e072127b13db4aa11f3744d94ce66dca
Kingsley Ochu <kochu@c2gconsulting.com>
2016-10-28 09:27:12 +0100

    add env

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -3,18 +3,18 @@ set -x
 
 : ${NODE_VERSION?"NODE_VERSION has not been set."}
 
+docker login -e ochu.kc@gmail.com -u chitezh -p ochu4real
+
 docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "chitezh/meteord:base-node-${NODE_VERSION}" ../base && \
   docker tag "chitezh/meteord:base-node-${NODE_VERSION}" chitezh/meteord:base
+  docker push chitezh/meteord:base
 docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "chitezh/meteord:onbuild-node-${NODE_VERSION}" ../onbuild && \
   docker tag "chitezh/meteord:onbuild-node-${NODE_VERSION}" chitezh/meteord:onbuild
+  docker push chitezh/meteord:devbuild
 docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "chitezh/meteord:devbuild-node-${NODE_VERSION}" ../devbuild && \
   docker tag "chitezh/meteord:devbuild-node-${NODE_VERSION}" chitezh/meteord:devbuild
 docker build --no-cache --build-arg "NODE_VERSION=${NODE_VERSION}" -t "chitezh/meteord:binbuild-node-${NODE_VERSION}" ../binbuild && \
   docker tag "chitezh/meteord:binbuild-node-${NODE_VERSION}" chitezh/meteord:binbuild
+  docker push chitezh/meteord:latest
 
-docker login -e ochu.kc@gmail.com -u chitezh -p ochu4real
 
-
-docker push chitezh/meteord:base
-docker push chitezh/meteord:devbuild
-docker push chitezh/meteord:latest
